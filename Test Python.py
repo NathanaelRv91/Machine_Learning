fb09e2adf36720aae535831e01402e8c535093bd
Nate <nathanaelrv91@gmail.com>
2025-07-13 14:48:27 -0600

    HERE

--- Test Python.py
+++ Test Python.py
@@ -5,3 +5,7 @@ from scipy import stats
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.figure_factory as ff
+
+
+titanic_df = pd.read_csv('Titanic_Project/titanic.csv')
+print(titanic_df.head(12))
